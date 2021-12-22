dd1870b7fb2abd78f6e0ee04b05a6db9cad61f74
IhorStorozhok <storozhokigor@gmail.com>
2021-12-22 22:37:26 +0200

    second

---
+++ src/Components/ContactList/ContactsList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import s from "./ContactList.module.css";
+import PropTypes from "prop-types";
+
+const ContactList = ({ contacts, onDeleteButton }) => {
+  return (
+    <div>
+      <ul>
+        {contacts.map((contact) => {
+          return (
+            <li className={s.contactListItem} key={contact.id}>
+              <span>{contact.name}:</span>
+              <span> {contact.number}</span>
+              <button
+                className={s.deleteButton}
+                type="button"
+                id={contact.id}
+                onClick={onDeleteButton}
+              >
+                delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default ContactList;
+
+ContactList.propTypes = {
+  contacts: PropTypes.array,
+  onDeleteButton: PropTypes.func,
+};
